20c6df907a08411016122211cb9b3f1e8e5ce143
Nabin Shrestha <nabin.shrestha@renegadeinsurance.com>
2023-01-28 16:32:47 +0545

    feat: display question page

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -4,11 +4,22 @@ import Flex from "../components/Flex";
 import SectionForm from "../components/form/SectionForm";
 import Stack from "../components/Stack";
 import { questionSections } from "../data/questionSections";
+import { styled } from "../theme";
 
 interface IQuestionsPageProps {
   onBack: VoidFunction;
   onContinue: VoidFunction;
 }
+
+const QuestionCount = styled("p", {
+  fontFamily: "Roboto",
+  fontWeight: 500,
+  lineHeight: "2.4rem",
+  textAlign: "center",
+  color: "$primary",
+  letterSpacing: "0.2em",
+  textTransform: "uppercase",
+});
 
 const QuestionsPage = ({ onBack, onContinue }: IQuestionsPageProps) => {
   const [questionIndex, setQuestionIndex] = useState(0);
@@ -23,6 +34,9 @@ const QuestionsPage = ({ onBack, onContinue }: IQuestionsPageProps) => {
   return (
     <Stack css={{ gap: "4.8rem" }}>
       {/* progressbar */}
+      <QuestionCount>{`QUESTION ${questionIndex + 1} / ${
+        questionSections.length
+      }`}</QuestionCount>
       <SectionForm
         key={currentQuestionSection.name}
         section={currentQuestionSection}
