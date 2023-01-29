222720eedfed60741c9f57392e03c9a339fa4b7f
Nabin Shrestha <nabin.shrestha@renegadeinsurance.com>
2023-01-29 14:06:19 +0545

    fix: mistake in end page description

--- src/pages/EndPage.tsx
+++ src/pages/EndPage.tsx
@@ -1,7 +1,7 @@
 import Heading2 from "../components/Heading2";
 
 const EndPage = () => {
-  return <Heading2>Thank you for completing our survey</Heading2>;
+  return <Heading2>Thank you for completing our survey!</Heading2>;
 };
 
 export default EndPage;
